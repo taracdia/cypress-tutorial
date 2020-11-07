e7b5b13971aa4fc262e646ef66ac0f50cbd51873
Tara Dia <taracdia@gmail.com>
2020-11-06 20:34:08 -0600

    tested the rest of the inputs

--- cypress/integration/form.spec.js
+++ cypress/integration/form.spec.js
@@ -2,5 +2,20 @@ describe("Form test", () => {
     it("Can fill the form", () => {
         cy.visit("/");
         cy.get("form");
+
+        cy.get("input[name='name']")
+            .type("Molly")
+            .should("have.value", "Molly");
+
+            cy.get('input[name="email"]')
+            .type("molly@dev.dev")
+            .should("have.value", "molly@dev.dev");
+      
+          cy.get("textarea")
+            .type("Mind you if I ask some silly question?")
+            .should("have.value", "Mind you if I ask some silly question?");
+
+        cy.get("form").submit();
+        
     });
 });
