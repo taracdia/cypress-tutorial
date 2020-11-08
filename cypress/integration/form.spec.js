9aec5980633d7048adb7fa94dab81827368647b3
Tara Dia <taracdia@gmail.com>
2020-11-08 07:55:02 -0600

    added stubbing (expected to fail)

--- cypress/integration/form.spec.js
+++ cypress/integration/form.spec.js
@@ -7,15 +7,26 @@ describe("Form test", () => {
             .type("Molly")
             .should("have.value", "Molly");
 
-            cy.get('input[name="email"]')
+        cy.get('input[name="email"]')
             .type("molly@dev.dev")
             .should("have.value", "molly@dev.dev");
-      
-          cy.get("textarea")
+
+        cy.get("textarea")
             .type("Mind you if I ask some silly question?")
             .should("have.value", "Mind you if I ask some silly question?");
 
         cy.get("form").submit();
-        
+
+        cy.server();
+        cy.route({
+            url: "/users/***",
+            method: "POST",
+            response: { status: "Form saved!", code: 201 }
+        });
+
+        cy.get("form").submit();
+
+        cy.contains("Form saved!")
+
     });
 });
